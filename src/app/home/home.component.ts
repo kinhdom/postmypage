cd0261eaab1d6e9c13fecdcf4b7fb064bcfa7794
Xuan Huy Dev <nguyenxuanhuy102@gmail.com>
2018-03-27 21:26:36 +0700

    observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
 import { PostcontentService } from '../service/postcontent.service';
 
+import { mergeMap } from 'rxjs/operators';
+
 const localToken = 'admin'
 
 @Component({
@@ -28,6 +30,20 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.arrPages = this._db.list('postmypage/users/' + localToken + '/pages').valueChanges()
+    let arrImages = ['https://dantricdn.com/2017/thu-1504399379412.jpg', 'https://donghuongkontum.files.wordpress.com/2015/09/mc3b9a-thu-vc3a0-c3a1o-tre1baafng.jpeg'];
+    let access_token = 'EAAAAUaZA8jlABABmKbLZCdnisGcPKRICrPICC6ZBN0uAIWSCTaC5f8m7ZCgvnrL4a8kEBwf5KVmkoAy6SavclJszD100OSKgHlJjG7YRWfdXxcxm6DowahpLrPL6RfkvAnb2bXcyZBg8rW4m2g57dyp2qDnfPMsHSLGoKfVPiUwZDZD'
+    let huy = this._postcontentservice.upImages(arrImages, access_token)
+    console.log(huy[0])
+    let van = Observable.from(huy[0])
+    console.log(van)
+    console.log(typeof van + 'van')
+    // huy[0].mergeMap(event1 => {
+    //   return huy[1].map(event2 => {
+    //     console.log(event1)
+    //     console.log(event2)
+    //   }).subscribe(res => console.log(res))
+    // })
+
   }
   onFileSelected(event) {
     this.attached_media = [];
@@ -41,16 +57,15 @@ export class HomeComponent implements OnInit {
       })
       taskUpload.downloadURL().subscribe(urlImage => {
         this.arrImages.push(urlImage)
-        let option = {
-          access_token: 'EAAAAUaZA8jlABAFeaSeev9b8RuRUdZBg1Ov4xzTyrSABHpUtKucFtqUpCtVacRkRllbFTQrKGTPZA7EtFCt1jYsWP7AWwPB9CA5Ek1BKrmUwC5O8ZCtakpO5IVdj4sJK8mTZCAfoOd42MB8Q0A2kt3cK1u6DEBS4gFZBh3pFziNgZDZD',
-          published: false,
-          url: urlImage
-        }
-        this._http.post('https://graph.facebook.com/v2.11/me/photos', option).subscribe(res => {
-
-          this.attached_media.push({ media_fbid: res.json().id })
-          console.log(res.json())
-        })
+        // let option = {
+        //   access_token: 'EAAAAUaZA8jlABAFeaSeev9b8RuRUdZBg1Ov4xzTyrSABHpUtKucFtqUpCtVacRkRllbFTQrKGTPZA7EtFCt1jYsWP7AWwPB9CA5Ek1BKrmUwC5O8ZCtakpO5IVdj4sJK8mTZCAfoOd42MB8Q0A2kt3cK1u6DEBS4gFZBh3pFziNgZDZD',
+        //   published: false,
+        //   url: urlImage
+        // }
+        // this._http.post('https://graph.facebook.com/v2.11/me/photos', option).subscribe(res => {
+        //   this.attached_media.push({ media_fbid: res.json().id })
+        //   console.log(res.json())
+        // })
       })
     }
   }
@@ -63,11 +78,8 @@ export class HomeComponent implements OnInit {
     Object.keys(form.value).map(access_token => {
       if (formvalue[access_token] === true) {
         // [{"media_fbid":"838987192964373"},{"media_fbid":"838987192964373"}]
-        this._postcontentservice.postContent(content, this.attached_media, access_token)
-        .subscribe(res => {
-          this.arrPosted.push(res)
-          console.log(res)
-        })
+        this._postcontentservice.postContent(content, this.arrImages, access_token)
+        // this._postcontentservice.postContent(content, this.arrImages, access_token)
       }
     })
   }
--- src/app/service/postcontent.service.ts
+++ src/app/service/postcontent.service.ts
@@ -1,22 +1,87 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { async } from '@firebase/util';
+import { forEachAsync } from 'forEachAsync';
 
+const localToken = localStorage.getItem('token')
 @Injectable()
 export class PostcontentService {
-
-  constructor(private _http: Http) { }
-  postUnpublish(image, access_token) {
-    // Return media_object_id
-  }
-  postContent(content, attached_media, access_token) {
-    let query = 'https://graph.facebook.com/v2.11/me/feed'
+  attached_media = [];
+  constructor(private _http: Http, private _db: AngularFireDatabase) { }
+  upOneImage(url_image, access_token) {
     let option = {
       access_token: access_token,
-      message: content,
-      attached_media: JSON.stringify(attached_media)
+      published: false,
+      url: url_image
     }
-    console.log(option)
-    return this._http.post(query, option).map(res => res.json())
-
+    return this._http.post('https://graph.facebook.com/v2.11/me/photos', option)
   }
+  upImages(arrImages, access_token) {
+    let arrRes = []
+    arrImages.forEach(async image => {
+      arrRes.push(this.upOneImage(image, access_token))
+    });
+    return arrRes
+  }
+
+  createArr(res) {
+    let arr = []
+    res.forEach(async element => {
+      console.log('before')
+      let huy = await this.convert(element)
+      console.log(huy)
+      console.log('after')
+    });
+  }
+  convert(dataObservable) {
+    return Promise.resolve(dataObservable.subscribe(res => res.json()))
+  }
+  // upimage(arrImages) //Return arr obj_fbid
+  // .then(upContent)
+  // .catch(ket_qua)
+
+
+  postContent(content, arrImages, access_token) {
+    let arr_attached_media = []
+    arrImages.forEach((image, index) => {
+      let option = {
+        access_token: access_token,
+        published: false,
+        url: image
+      }
+      this._http.post('https://graph.facebook.com/v2.11/me/photos', option).subscribe(res => {
+        arr_attached_media.push({ media_fbid: res.json().id })
+      })
+      if (index == arrImages.length - 1) {
+        let option_feed = {
+          access_token: access_token,
+          message: content,
+          attached_media: arr_attached_media
+        }
+        let query = 'https://graph.facebook.com/v2.11/me/feed'
+        this._http.post(query, option_feed).map(res => res.json()).subscribe(res => {
+          console.log(res)
+        })
+      }
+    });
+  }
+}
+
+
+
+
+
+
+
+
+function getIdImage(arrayImage) {
+  let arrayIdImage = [];
+  arrayImage.forEach(image => {
+    this._http.post('myserver.com', {}).subscribe(id => {
+      arrayIdImage.push(id)
+    })
+  });
+  return arrayIdImage;
 }
